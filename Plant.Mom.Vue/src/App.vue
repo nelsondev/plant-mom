4d097b7da97d2a74183c726ebe6d6ab6ab3506d1
Nelson M <nelsontron@outlook.com>
2022-02-10 00:14:43 -0700

    added start of front end

---
+++ Plant.Mom.Vue/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <v-app>
+    <v-app-bar
+    app
+    flat
+    color="white"
+    >
+      <v-toolbar-items>
+        <v-btn plain color="white">
+          <v-icon color="deep-purple">mdi-menu</v-icon>
+        </v-btn>
+      </v-toolbar-items>
+      <v-toolbar-title class="deep-purple px-1">
+        <span class="font-logo white--text">Plant Mom</span>
+        <v-icon color="white" class="pl-2" size="22pt">mdi-sprout</v-icon>
+      </v-toolbar-title>
+      
+    </v-app-bar>
+    <v-main class="grey lighten-4">
+      <router-view></router-view>
+    </v-main>
+  </v-app>
+</template>
+
+<style>
+.font-logo {
+  font-family: 'Lora', serif !important;
+  font-weight: 700 !important;
+  letter-spacing: -1px;
+}
+</style>
