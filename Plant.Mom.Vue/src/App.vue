4fe6d2d3ca607d9ade425ea50bad922d01b40a17
Nelson M <nelsontron@outlook.com>
2022-02-10 00:24:30 -0700

    Add component

--- Plant.Mom.Vue/src/App.vue
+++ Plant.Mom.Vue/src/App.vue
@@ -1,26 +1,20 @@
 <template>
   <v-app>
-    <v-app-bar
-    app
-    flat
-    color="white"
-    >
-      <v-toolbar-items>
-        <v-btn plain color="white">
-          <v-icon color="deep-purple">mdi-menu</v-icon>
-        </v-btn>
-      </v-toolbar-items>
-      <v-toolbar-title class="deep-purple px-1">
-        <span class="font-logo white--text">Plant Mom</span>
-        <v-icon color="white" class="pl-2" size="22pt">mdi-sprout</v-icon>
-      </v-toolbar-title>
-      
-    </v-app-bar>
+    <Menu />
     <v-main class="grey lighten-4">
       <router-view></router-view>
     </v-main>
   </v-app>
 </template>
+
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({
+  components: {
+    Menu: () => import("@/components/Menu.vue")
+  }
+})
+</script>
 
 <style>
 .font-logo {
---
+++ Plant.Mom.Vue/src/components/Menu.vue
@@ -0,0 +1,13 @@
+<template>
+    <v-app-bar
+    app
+    flat
+    color="white"
+    class="d-flex justify-center"
+    >
+      <v-toolbar-title class="deep-purple px-1">
+        <span class="font-logo white--text">Plant Mom</span>
+        <v-icon color="white" class="pl-2" size="22pt">mdi-sprout</v-icon>
+      </v-toolbar-title>
+    </v-app-bar>
+</template>
